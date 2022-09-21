8b92aaf45853110253c9c25df873f25abe5e1c64
MihaelaGrn <arond_com@yahoo.com>
2022-09-21 15:57:47 +0300

    finalizare proiect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const city = localStorage.getItem("city");
+const scrollToTopButton = document.querySelector(".scroll-to-top");
 
 if (city === null) {
   localStorage.setItem("city", "București");
@@ -9,3 +10,18 @@ if (city === null) {
   currentCity.innerText = city;
   displayWeatherForecast(city);
 }
+
+scrollToTopButton.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+document.addEventListener("scroll", function () {
+  if (window.scrollY > 500) {
+    scrollToTopButton.style.visibility = "visible";
+  } else {
+    scrollToTopButton.style.visibility = "hidden";
+  }
+});
